a2c08c3a592ee05e8f7694f3a07b7971bb26c16e
VictorClvtt <joaoclivatti@hotmail.com>
2026-03-30 23:16:29 -0300

    refactor: restructure and update the homepage's html and css files

--- apps/core/views.py
+++ apps/core/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'global/base.html')
+    return render(request, 'core/home.html')
