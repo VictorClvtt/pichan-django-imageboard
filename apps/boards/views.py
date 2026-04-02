1f22220ac95dc96a9b809893a601ad2c9b3646e7
VictorClvtt <joaoclivatti@hotmail.com>
2026-04-01 23:09:13 -0300

    feat: implement individual board pages

--- apps/boards/views.py
+++ apps/boards/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import View
+from . import models
 
-# Create your views here.
+class Board(View):
+    
+    def get(self, request, slug):
+        
+
+        context = {
+            "current_board": get_object_or_404(models.Board, slug=slug),
+            "slugs": models.Board.objects.values_list('slug', flat=True)
+        }
+
+        return render(request, 'boards/board.html', context)
